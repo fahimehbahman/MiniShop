e943b2fa4597ed730b82cd00406daef525b5a6ea
fahimehbahman <fahimeh.bahman@gmail.com>
2026-03-21 22:34:29 +0100

    test ci v1

--- MiniShop.Api/Program.cs
+++ MiniShop.Api/Program.cs
@@ -22,7 +22,7 @@ using MiniShop.Api.Mapping;
 var builder = WebApplication.CreateBuilder(args);
 
 // =====================
-// Add services ---
+// Add services ----
 // =====================
 
 builder.Services.AddControllers();
