b1bd46862d7bdc412f239fbdb2fb5b0ce8351dc0
fahimehbahman <fahimeh.bahman@gmail.com>
2026-03-22 13:49:02 +0100

    add migration image build

--- MiniShop.Api/Program.cs
+++ MiniShop.Api/Program.cs
@@ -22,7 +22,7 @@ using MiniShop.Api.Mapping;
 var builder = WebApplication.CreateBuilder(args);
 
 // =====================
-// Add services ----
+// Add services ----v1
 // =====================
 
 builder.Services.AddControllers();
